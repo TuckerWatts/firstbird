c8e4e4cb9b7e433161d30ff27cfd3732cdcfee88
Tucker Watts <tuckerwatts@mail.com>
2024-12-19 13:11:35 -0500

    add ml_score to stocks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_23_200359) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_19_181118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_23_200359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "latest_price"
+    t.decimal "ml_score"
   end
 
   create_table "top_stocks", force: :cascade do |t|
